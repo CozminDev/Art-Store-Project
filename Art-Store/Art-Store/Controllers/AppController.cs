3a47f1d634c904b63248b4997bb3cf6a8af1e6bc
CozminDev <37241019+CozminDev@users.noreply.github.com>
2018-04-03 16:40:04 +0300

    Bootstrap prototype and basic MVC

---
+++ Art-Store/Art-Store/Controllers/AppController.cs
@@ -0,0 +1,38 @@
+﻿using ArtStore.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtStore.Controllers
+{
+    public class AppController:Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+          
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.UserMessage = "Message sent";
+                ModelState.Clear();
+            }
+            return View();
+        }
+    }
+}
---
+++ Art-Store/Art-Store/ViewModels/ContactViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtStore.ViewModels
+{
+    public class ContactViewModel
+    {
+        [Required]
+        [MinLength(4)]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        [MinLength(5)]
+        public string Message { get; set; }
+    }
+}
